80e23b9beef535cc8a58cfab8dd145fafedeafb8
Octavio-DiazG <odiazg01@gmail.com>
2022-11-18 08:07:02 -0600

    Player response when idle and movement, changes animation in corresponsive time

--- lib/components/player_component.dart
+++ lib/components/player_component.dart
@@ -6,19 +6,35 @@ import 'package:minecraft2d_game/global/player_data.dart';
 
 class PlayerComponent extends SpriteAnimationComponent{
 
-
+  final Vector2 playerDimensions = Vector2(50, 40); //the number of pixels it will be cut the spriteSheet
+  //final double stepTime = 0.15; maybe use it later
   final double speed = 5;
   bool isFacingRight = true;
+
+  // Movement Animation
+  late SpriteSheet playerWalkingSpritesheet;
+  late SpriteAnimation walkingAnimation = playerWalkingSpritesheet.createAnimation(row: 0, stepTime: 0.12);
+
+  // Idle Animation
+  late SpriteSheet playerIdleSpritesheet;
+  late SpriteAnimation idleAnimation = playerIdleSpritesheet.createAnimation(row: 0, stepTime: 0.18);
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    SpriteSheet playerSpriteSheet = SpriteSheet(
+
+    //WalkingSprite
+    playerWalkingSpritesheet = SpriteSheet(
       image: await Flame.images.load('sprite_sheets/own_imports/RougeHeroWalk.png'), //tells flutter where to grab the spriteSheet
       srcSize: Vector2(50,45), //the number of pixels it will be cut the spriteSheet
     );
+    //IdleSprite 
+    playerIdleSpritesheet = SpriteSheet(
+      image: await Flame.images.load('sprite_sheets/own_imports/RougeHeroIdle.png'), //tells flutter where to grab the spriteSheet
+      srcSize: Vector2(50,45), //the number of pixels it will be cut the spriteSheet
+    );
 
-    animation = playerSpriteSheet.createAnimation(row: 0, stepTime: 0.13); //set the animation row->what row it will take from the spritesheet. stepTime->time between the sprites 
+    animation = idleAnimation; //set the animation row->what row it will take from the spritesheet. stepTime->time between the sprites 
     size = Vector2(100,100);
     position = Vector2(100,400); //position in the world
   }
@@ -38,6 +54,7 @@ class PlayerComponent extends SpriteAnimationComponent{
         flipHorizontallyAroundCenter();
         isFacingRight = false;
       }
+      animation = walkingAnimation;
     }
     //Moving Right
     if(GlobalGameReference.instance.gameReference.worldData.playerData.componentMotionState == ComponentMotionState.walkingRight){
@@ -46,6 +63,11 @@ class PlayerComponent extends SpriteAnimationComponent{
         flipHorizontallyAroundCenter();
         isFacingRight = true;
       }
+      animation = walkingAnimation;
+    }
+    //Idle 
+    if (GlobalGameReference.instance.gameReference.worldData.playerData.componentMotionState == ComponentMotionState.idle) {
+      animation = idleAnimation;
     }
   }
 }
